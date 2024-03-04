40902b6b65efc712c76ba8ae3c01f9345c5a11b8
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-04 12:59:52 -0300

    Initial commit

---
+++ src/main/java/br/com/bossini/teste_gitlab_api_livros/TesteGitlabApiLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.bossini.teste_gitlab_api_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGitlabApiLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteGitlabApiLivrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bossini/teste_gitlab_api_livros/TesteGitlabApiLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bossini.teste_gitlab_api_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGitlabApiLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
